bebc6ef6fa33883ef6ee7fdbf4c58044f2e39717
NiMadeNai <e0424663@u.nus.edu>
2020-09-29 17:22:15 +0800

    Add Remark Command

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -22,17 +22,20 @@ public class Person {
 
     // Data fields
     private final Address address;
+    private final Remark remark;
+
     private final Set<Tag> tags = new HashSet<>();
 
     /**
      * Every field must be present and not null.
      */
-    public Person(Name name, Phone phone, Email email, Address address, Set<Tag> tags) {
+    public Person(Name name, Phone phone, Email email, Address address, Remark remark, Set<Tag> tags) {
         requireAllNonNull(name, phone, email, address, tags);
         this.name = name;
         this.phone = phone;
         this.email = email;
         this.address = address;
+        this.remark = remark;
         this.tags.addAll(tags);
     }
 
@@ -49,7 +52,9 @@ public class Person {
     }
 
     public Address getAddress() {
-        return address;
+        return address;}
+    public Remark getRemark(){
+        return remark;
     }
 
     /**
@@ -93,6 +98,7 @@ public class Person {
                 && otherPerson.getPhone().equals(getPhone())
                 && otherPerson.getEmail().equals(getEmail())
                 && otherPerson.getAddress().equals(getAddress())
+                && otherPerson.getRemark().equals((getRemark()))
                 && otherPerson.getTags().equals(getTags());
     }
 
@@ -112,6 +118,8 @@ public class Person {
                 .append(getEmail())
                 .append(" Address: ")
                 .append(getAddress())
+                .append(" Remarks: ")
+                .append(getRemark())
                 .append(" Tags: ");
         getTags().forEach(builder::append);
         return builder.toString();
